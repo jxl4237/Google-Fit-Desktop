e75a01848509176756723c6ae3702214d7bf01ae
Jacob Lambert <jlambert44@gatech.edu>
2021-02-22 14:10:11 -0500

    updated to typescript

--- my-app/src/components/App.tsx
+++ my-app/src/components/App.tsx
@@ -1,7 +1,8 @@
+import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
+export default function App() : JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,5 +22,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
